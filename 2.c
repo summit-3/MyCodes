01fe4dc250dff4e02b3134b78429281118f387dc
Sumit Keshari <70266090+summit-3@users.noreply.github.com>
2020-09-29 12:18:59 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,27 @@
+//Maximum Rent
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int t;
+	scanf("%d", &t);
+	while (t--) {
+		int n;
+		scanf("%d", &n);
+		int arr[n];
+		for (int i = 0; i < n; i++)
+			scanf("%d", &arr[i]);
+		int res = 0;
+		for (int i = 0; i < n; i++) {
+			if (arr[i] > res) {
+				res = arr[i];
+			}
+		}
+		printf("%d", res);
+		printf("\n");
+
+	}
+
+	return 0;
+}
