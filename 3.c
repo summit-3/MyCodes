fd920646185847bf6201235ac8ceaca32728841c
Sumit Keshari <70266090+summit-3@users.noreply.github.com>
2020-10-04 10:55:59 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -1,9 +1,51 @@
 //Prime Fibonacci 
 
 #include<stdio.h>
+#define MAX 1000
+void fibonacci(int n)
+{
+	int i, t1 = 0, t2 = 1, nextTerm;
+	for (i = 1; i<=n; i++)
+	{
+		nextTerm = t1 + t2;
+		t1 = t2;
+		t2 = nextTerm;
+	}
+	printf("%d", t1);
+}
+
+void prime(int n)
+{
+	int i, j, flag, count =0;
+	for (i=2; i<=MAX; i++)
+	{
+		flag = 0;
+		for (j=2; j<i; j++)
+		{
+			if(i%j == 0)
+			{
+				flag = 1;
+				break;
+			}
+		}
+		if (flag == 0)
+		{
+			if(++count == n)
+			{
+				printf("%d", i);
+				break;
+			}
+		}	
+	}
+}
+
 int main()
 {
-  //You try your own first. I will definately post the solution.
-
+	int n;
+	scanf("%d", &n);
+	if(n%2 == 1)
+		fibonacci (n/2 + 1);
+	else
+		prime(n/2);
 	return 0;
 }
